4bee622570ffc2243bc6a8134356902519656281
bairariyakhushal <bairariyakhushal@gmail.com>
2025-09-07 11:38:01 +0530

    Fix CORS configuration for frontend URL

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ database.dbConnect();
 
 // Middleware
 app.use(cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000",
+		"https://eduelevate-ai.vercel.app"],
 		credentials:true,
 	}));
 app.use(express.json());
